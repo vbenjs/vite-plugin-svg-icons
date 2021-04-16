8ba190d6174e34684867355aff49dea52a8ecb83
Vben <anncwb@126.com>
2021-04-16 23:45:07 +0800

    chore: release 0.4.3

--- client.d.ts
+++ client.d.ts
@@ -2,3 +2,7 @@ declare module 'vite-plugin-svg-icons/client' {
   const result: string[];
   export default result;
 }
+declare module 'virtual:svg-icons-names' {
+  const result: string[];
+  export default result;
+}
