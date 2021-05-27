c1edb2461cce7e056a8662517e68c948ef38df2a
yangmi <yangmi0829@vip.qq.com>
2021-05-27 23:37:57 +0800

    fix bug:    linearGradient svg's color looks changed

--- example/src/App.vue
+++ example/src/App.vue
@@ -3,6 +3,7 @@
     <SvgIcon name="icon1"></SvgIcon>
     <SvgIcon name="icon2"></SvgIcon>
     <SvgIcon name="icon3"></SvgIcon>
+    <SvgIcon name="icon4"></SvgIcon>
     <SvgIcon name="dir-icon1"></SvgIcon>
     <SvgIcon name="color" color="#8B81C3"></SvgIcon>
   </div>
