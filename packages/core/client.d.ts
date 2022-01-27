23125d4aca84f21524068bd263d64f2941a9ae8b
vben <anncwb@126.com>
2022-01-28 00:40:20 +0800

    feat: support typescript,close #22

---
+++ packages/core/client.d.ts
@@ -0,0 +1,11 @@
+declare module 'virtual:svg-icons-register' {
+  // eslint-disable-next-line
+  const component: any
+  export default component
+}
+
+declare module 'virtual:svg-icons-names' {
+  // eslint-disable-next-line
+  const iconsNames: string[]
+  export default iconsNames
+}
